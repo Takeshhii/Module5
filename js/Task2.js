67770cebe09d0030027a529b1172399655bf6e62
Takeshi <111451720+Takeshhii@users.noreply.github.com>
2022-10-01 06:27:44 -0700

    12

---
+++ js/Task2.js
@@ -0,0 +1,10 @@
+let x = -1;
+if (typeof x == "number"){
+    console.log(x + " - число");
+}
+if (typeof x == "string"){
+    console.log(x + " - строка");
+}
+if (typeof x == "boolean"){
+    console.log(x + " - логический тип");
+}
