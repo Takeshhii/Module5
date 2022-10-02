06711e563d21a12febb98008a30b0a7dcee0f45a
Takeshi <111451720+Takeshhii@users.noreply.github.com>
2022-10-02 05:13:57 -0700

    17

---
+++ js/Task8.js
@@ -0,0 +1,12 @@
+let fruits = new Map([
+
+    ["apple", "green"],
+
+    ["strawberry", "red"],
+
+    ["blueberry", "blue"]
+
+]);
+for (let [key, value] of fruits){
+    console.log(`Ключ — ${key}, значение — ${value}`);
+}
